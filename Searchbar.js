a2408159454ddc44969e6aa239ed2129d009b89c
Timur Uzel <timur@Timurs-MacBook-Pro.local>
2017-01-01 03:53:46 -0500

    first

---
+++ Searchbar.js
@@ -0,0 +1,15 @@
+/**
+ * Created by timur on 1/1/17.
+ */
+
+const React = require('react')
+
+export default class extends React.Component {
+  
+  render() {
+    return(
+      <input type="text"/>
+    )
+  }
+  
+}
--- index.js
+++ index.js
@@ -2,6 +2,6 @@
  * Created by timur on 1/1/17.
  */
 
-console.log(
-  'hello'
+module.exports = (
+  require('./Searchbar')
 )
